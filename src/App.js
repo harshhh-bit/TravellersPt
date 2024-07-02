582969998969ee992b4df6c215dcd9f6b95202be
Harshit Goel <harshitgoel003@gmail.com>
2024-07-02 17:03:35 +0530

    React Mapbox used

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import * as React from 'react';
+
+import Map from 'react-map-gl';
+
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Map
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      initialViewState={{
+        longitude: +77,
+        latitude: +20,
+        zoom: 4
+      }}
+      style={{width: "100vw", height: "100vh"}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
   );
 }
 
