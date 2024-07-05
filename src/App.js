f69e4f4b178964ef2c7c9aaa9ec51091fe5d2ea4
Harshit Goel <harshitgoel003@gmail.com>
2024-07-06 00:23:48 +0530

    Header added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import * as React from 'react';
 
-import Map from 'react-map-gl';
+import { CssBaseline } from '@material-ui/core'
+
+import Header from './components/Header/Header'
+import Map, {Marker} from 'react-map-gl';
+import TourIcon from '@mui/icons-material/Tour';
 
 function App() {
   return (
-    <Map
-      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-      initialViewState={{
-        longitude: +77,
-        latitude: +20,
-        zoom: 4
-      }}
-      style={{width: "100vw", height: "100vh"}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+    <>
+      <CssBaseline />
+      <Header />
+      <Map
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+        initialViewState={{
+          longitude: +77.12,
+          latitude: +28.38,
+          zoom: 4
+        }}
+        style={{width: "100vw", height: "91.5vh"}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+        <Marker longitude={77} latitude={20} anchor="bottom" >
+          <TourIcon style={{ color: "slateblue" }} />
+        </Marker>
+      </Map>
+    </>
   );
 }
 
 export default App;
+// 1. TourIcon size dynamics 2. Map style update
