97ffe0dd02b517ac609fadcd7655f47957a63a11
Harshit Goel <harshitgoel003@gmail.com>
2024-07-06 17:05:28 +0530

    React fetching data from Node.js using Axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import * as React from 'react';
 
 import { CssBaseline } from '@material-ui/core'
-
 import Header from './components/Header/Header'
-import Map, {Marker, Popup} from 'react-map-gl';
+import Map, { Marker, Popup } from 'react-map-gl';
 import TourIcon from '@mui/icons-material/Tour';
+import { format } from "timeago.js"
 
 import "./app.css"
 
+import axios from "axios";
+
+
 function App() {
+  const currentUser = "harshit";
+  const [travels, setTravels] = React.useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = React.useState(null);
+  
+  React.useEffect(() => {
+    const getTravels = async () => {
+      try {
+        const res= await axios.get("/travels");
+        setTravels(res.data);
+      } catch(err) {
+        console.log(err);
+      }
+    };
+    getTravels();
+  }, []);
+
+  const handleMarkerClick = (id) => {
+    setCurrentPlaceId(id);
+  }
+
   return (
     <>
       <CssBaseline />
@@ -23,27 +46,44 @@ function App() {
         style={{width: "100vw", height: "91.5vh"}}
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
-        <Marker longitude={77} latitude={20} anchor="bottom" >
-          <TourIcon style={{ color: "slateblue" }} />
-        </Marker>
-        <Popup 
-          longitude={77} 
-          latitude={20}
-          anchor="left">
-          <div className="card">
-            <label>Title</label>
-            <h3 className="title">J&K Diaries</h3>
-            <label>Blog</label>
-            <p className="blog">Beautiful Place. I like it.</p>
-            <label>Information</label>
-            <span className="username">Created by <b>harshit</b></span>
-            <span className="userbio">Travel Enthusiast</span>
-          </div>
-        </Popup>
+        {travels.map(travel => (
+          <>
+            <Marker longitude={travel.long} latitude={travel.lat} anchor="bottom" >
+              <TourIcon 
+                style={{ 
+                  color: travel.username === currentUser ? "tomato" : "slateblue", 
+                  cursor: "pointer" 
+                }} 
+                onClick={() => handleMarkerClick(travel._id)}
+              />
+            </Marker>
+            {travel._id === currentPlaceId && (
+              <Popup
+                longitude={travel.long} 
+                latitude={travel.lat}
+                anchor="left"
+                closeButton={true}
+                closeOnClick={false}
+                onClose={() => setCurrentPlaceId(null)}>
+                <div className="card">
+                  <label>Title</label>
+                  <h3 className="title">{travel.title}</h3>
+                  <label>Blog</label>
+                  <p className="blog">{travel.blog}</p>
+                  <label>Information</label>
+                  <span className="username">Created by <b>{travel.username}</b></span>
+                  <span className="userbio">Travel Enthusiast</span>
+                  <span className="date">{format(travel.createdAt)}</span>  
+                </div>
+              </Popup>
+            )}
+          </>
+        ))}
       </Map>
     </>
   );
 }
 
 export default App;
+
 // 1. TourIcon size dynamics 2. Map style update
